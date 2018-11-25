ceaa40c79fd7a5c371372649660c58ca868425ea
Silver <rbpsilver@gmail.com>
2018-11-25 17:09:06 -0200

    add nginx
    Update docker-compose, added nginx,  added logging with winston + morgan

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -2,14 +2,18 @@ const express = require('express')
 const app = express()
 const httpProxy = require('express-http-proxy')
 const cookieParser = require('cookie-parser');
-const logger = require('morgan');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
-const helper = require('./helper');
+const helper = require('./helpers/auth');
+const morgan = require('morgan');
+const winston = require('./helpers/winston');
+
+const proxyUser = httpProxy(process.env.PROXY_USER);
+const proxyModule = httpProxy(process.env.PROXY_MODULE);
+const proxyLog = httpProxy(process.env.PROXY_LOG);
 
 require('dotenv-safe').load();
 
-app.use(logger('dev'));
 app.use(helmet());
 app.use(cookieParser());
 
@@ -17,6 +21,8 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
+
+app.use(morgan('combined', { stream: winston.stream }));
 
 // cors
 app.use((req, res, next) => {
@@ -26,10 +32,6 @@ app.use((req, res, next) => {
     next();
 });
 
-const proxyUser = httpProxy(process.env.PROXY_USER);
-const proxyModule = httpProxy(process.env.PROXY_MODULE);
-const proxyLog = httpProxy(process.env.PROXY_LOG);
-
 app.post('/login', httpProxy(process.env.PROXY_USER, {
     userResDecorator: function(proxyRes, proxyResData, userReq, userRes) {
         let s = proxyResData.toString('utf8');
@@ -38,6 +40,20 @@ app.post('/login', httpProxy(process.env.PROXY_USER, {
         return JSON.stringify(res);
     }
 }));
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // add this line to include winston logging
+    winston.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
 
 app.use('/users', helper.verifyJWT, (req, res, next) => proxyUser(req, res, next));
 
